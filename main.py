0842c527f9bf14beda23df28c74c38c381fac9c9
hksharma2004 <hksharma2004@users.noreply.github.com>
2026-03-15 13:44:59 +0530

    change model

--- main.py
+++ main.py
@@ -111,7 +111,7 @@ def get_conversational_chain():
     Answer:
     """
     # initializing model
-    model = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0.3)
+    model = ChatGoogleGenerativeAI(model="gemini-3-flash", temperature=0.3)
     prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
     
     # prompt chaining here
